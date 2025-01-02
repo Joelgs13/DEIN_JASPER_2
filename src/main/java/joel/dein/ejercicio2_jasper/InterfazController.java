0aaf32e106e4f8155a9ee5d63b3fe033a6642c55
Joelgs13 <050213jgonzalez@gmail.com>
2025-01-02 11:10:25 +0100

    añadir lo necesario a el proyecto antes de diseñar los informes

--- src/main/java/joel/dein/ejercicio2_jasper/InterfazController.java
+++ src/main/java/joel/dein/ejercicio2_jasper/InterfazController.java
@@ -1,14 +1,21 @@
 package joel.dein.ejercicio2_jasper;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class InterfazController {
     @FXML
     private Label welcomeText;
 
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    public void Aceptar(ActionEvent event) {
+    }
+
+    public void Cancelar(ActionEvent event) {
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module joel.dein.ejercicio2_jasper {
     requires javafx.controls;
     requires javafx.fxml;
+    requires jasperreports;
+    requires java.sql;
 
 
     opens joel.dein.ejercicio2_jasper to javafx.fxml;
