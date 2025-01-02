1ee46fe7b7c3f65ceaba4914f085a96c9ba4706f
Joelgs13 <050213jgonzalez@gmail.com>
2025-01-02 14:10:10 +0100

    Primer informe terminado y añadido configuracion

--- src/main/java/joel/dein/ejercicio2_jasper/InterfazController.java
+++ src/main/java/joel/dein/ejercicio2_jasper/InterfazController.java
@@ -1,21 +1,80 @@
 package joel.dein.ejercicio2_jasper;
 
+import java.io.InputStream;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
+
+import BBDD.ConexionBBDD;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.util.JRLoader;
+import net.sf.jasperreports.view.JasperViewer;
 
 public class InterfazController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Button btAceptar;
+
+    @FXML
+    private Button btCancelar;
+
+    @FXML
+    private ToggleGroup grupoIformes;
+
+    @FXML
+    private RadioButton rbPersonas;
+
+    @FXML
+    private RadioButton rbPCalculos;
+
+    @FXML
+    private RadioButton rbPSubinformes;
+
+    private void generarReporte(String reportePath, Map<String, Object> parameters) {
+        try {
+            ConexionBBDD db = new ConexionBBDD();
+            InputStream reportStream = getClass().getResourceAsStream(reportePath);
+
+            if (reportStream == null) {
+                System.out.println("El archivo no está ahí: " + reportePath);
+                return;
+            }
+
+            JasperReport report = (JasperReport) JRLoader.loadObject(reportStream);
+            JasperPrint jprint = JasperFillManager.fillReport(report, parameters, db.getConnection());
+            JasperViewer viewer = new JasperViewer(jprint, false);
+            viewer.setVisible(true);
+
+        } catch (SQLException | JRException e) {
+            e.printStackTrace();
+        }
     }
 
-    public void Aceptar(ActionEvent event) {
+    @FXML
+    void Aceptar(ActionEvent event) {
+        if (rbPersonas.isSelected()) {
+            generarReporte("/JasperReport/InformePersonas.jasper", null);
+        } else if (rbPCalculos.isSelected()) {
+            Map<String, Object> parameters = new HashMap<>();
+            parameters.put("IMAGE_PATH", getClass().getResource("/imagenes/").toString());
+            generarReporte("/JasperReport/Agenda_InformePersonas_Calculos.jasper", parameters);
+        } else if (rbPSubinformes.isSelected()) {
+            Map<String, Object> parameters = new HashMap<>();
+            parameters.put("Resource_PATH", getClass().getResource("/jasper/").toString());
+            generarReporte("/JasperReport/Agenda_InformePersonas_Subinforme.jasper", parameters);
+        }
     }
 
-    public void Cancelar(ActionEvent event) {
+    @FXML
+    void Cancelar(ActionEvent event) {
+        System.exit(0); // O usa una referencia a la ventana principal si deseas cerrarla específicamente
     }
 }
